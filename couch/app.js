e49a77258d458cac379b0623ec842beb733fef33
Ryan Ramage <ryan.ramage@gmail.com>
2015-08-25 18:12:12 -0600

    add by_user_tag query

--- couch/app.js
+++ couch/app.js
@@ -60,6 +60,25 @@ ddoc.views.by_user_event_tag = {
   reduce: '_count'
 }
 
+ddoc.views.by_user_tag = {
+  map: function(doc) {
+    if (doc.type !== 'app-user-event') return;
+    if (!doc.tags) return;
+
+    // repeatitive
+    var d = new Date(doc.timestamp);
+    var year = Number(d.getFullYear());
+    var month = d.getMonth() + 1;
+    if (month <= 9) month = '0' + month;
+    var day = d.getDate() ;
+    if (day <=9) day = '0' + day;
+    for (var i = doc.tags.length - 1; i >= 0; i--) {
+      var tag = doc.tags[i];
+      emit([doc.space, doc.user_id, tag, Number(year), Number(month), Number(day), doc.timestamp, doc.event])
+    };
+  },
+  reduce: '_count'
+}
 
 ddoc.views.by_event = {
   map: function(doc) {
